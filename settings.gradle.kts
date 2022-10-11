34b3c157e45344927bb8f54dc07d186d00d54382
CunningBird <68229355+CunningBird@users.noreply.github.com>
2022-10-11 11:33:37 +0300

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,12 @@
+pluginManagement {
+    repositories {
+        maven {
+            url = uri(System.getProperty("publicPluginsRepositoryUrl"))
+        }
+    }
+}
+
+rootProject.name = "template-spring"
+
+// TODO build slim jar
+// TODO optimize image build
